e1fcbe18138886418859b70551bde7168699805a
Cheryl Lee <clee0937@gmail.com>
2025-04-03 12:00:02 +0100

    added getOrders() function to filter orders based on price

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,19 @@ const getMaxPrice = (price: PriceBracket): number => {
       throw new Error("Invalid price bracket value");
   }
 };
+
+// Function to filter the orders from each restaurant that cost below the maximum price
+const getOrders = (price: number, orders: Order[][]): Order[][] => {
+  let filteredOrders: Order[][] = [];
+
+  orders.forEach((restaurant) => {
+    filteredOrders.push(
+      restaurant.filter((order) => order.price <= getMaxPrice(price))
+    );
+  });
+
+  return filteredOrders;
+};
+
+const eligibleOrders = getOrders(PriceBracket.Low, orders);
+console.log(eligibleOrders);
